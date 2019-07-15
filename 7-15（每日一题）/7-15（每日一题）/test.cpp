f53ce2751bc243679164ba51d16a48f7bc0722bc
dpfnmt <1097722509@qq.com>
2019-07-15 19:57:45 +0800

    每日一题

---
+++ 7-15（每日一题）/7-15（每日一题）/test.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+
+	std::pair<K, V> _kv;
+	int _bf; // balance factor
+};
