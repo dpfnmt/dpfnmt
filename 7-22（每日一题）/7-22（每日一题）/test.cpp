3b09691326a686c2d14f3aa240b683be04bf9af0
dpfnmt <1097722509@qq.com>
2019-07-22 17:47:14 +0800

    每日一题测试代码

---
+++ 7-22（每日一题）/7-22（每日一题）/test.cpp
@@ -0,0 +1,17 @@
+/*int i = 0;
+int lenth = A.size()-1;
+while( lenth > 0 )
+{
+for(i = 0 ;i < lenth ;i++)
+{
+if(A[i] > A[i+1])
+{
+int tmp = A[i];
+A[i] = A[i+1];
+A[i+1]= tmp;
+}
+}
+lenth--;
+}
+cout<<A[n-1] - A[0] <<endl;*/
+
