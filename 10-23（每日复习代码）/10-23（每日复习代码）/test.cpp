8b1c913d7ae7e40e755eeb8bfae808d835e76896
dpfnmt <1097722509@qq.com>
2019-10-23 23:18:23 +0800

    c++复习部分

---
+++ 10-23（每日复习代码）/10-23（每日复习代码）/test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+public:
+	virtual void BuyTicket() { cout << "ÂòÆ±-È«¼Û" << endl; }
+};
+
+class Student : public Person {
+public:
+	virtual void BuyTicket() { cout << "ÂòÆ±-°ë¼Û" << endl; }
+};
+
+void Func(Person& p)
+{
+	p.BuyTicket();
+}
+
+int main()
+{
+	Person Mike;
+	Func(Mike);
+	Student Johnson;
+	Func(Johnson);
+	return 0;
+}
